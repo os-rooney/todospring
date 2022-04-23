b02dc20a213a6a8c7c436d3672c6a0bcce22f065
Rooney H. Osman <dev@ronios.de>
2022-04-23 11:48:56 +0200

    upload base code

---
+++ ToDoApp/src/main/java/de/ronios/ToDoApp/HomeController.java
@@ -0,0 +1,13 @@
+package de.ronios.ToDoApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
---
+++ ToDoApp/src/test/java/de/ronios/ToDoApp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ronios.ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
